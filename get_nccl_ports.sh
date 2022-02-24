d4eb36448845b19b831c88ee716e084630c6b6d6
BenoitPitClaudel <bpitcla@mit.edu>
2022-02-24 00:38:05 +0000

    Add absolute sequence numbers to tcp logging

--- get_nccl_ports.sh
+++ get_nccl_ports.sh
@@ -4,4 +4,4 @@ sleep 10s
 tcpdump -c 5000 -i enp194s0 -Q inout -l > nccl_ports
 ports=(`python get_dst_ports.py nccl_ports`)
 sleep 10s
-tcpdump -c 500000 -i enp194s0 -Q inout -l dst port ${ports[0]} or ${ports[1]} or ${ports[2]} or ${ports[3]} > $1
+tcpdump -S -c 500000 -i enp194s0 -Q inout -l dst port ${ports[0]} or ${ports[1]} or ${ports[2]} or ${ports[3]} > $1
