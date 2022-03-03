ef6a700257a837e2c13e0d8b7f805b3bd3113f0b
BenoitPitClaudel <bpitcla@mit.edu>
2022-03-03 02:28:32 +0000

    changed packet delay computation algorithm for performance

--- 3servers-22-23.sh
+++ 3servers-22-23.sh
@@ -12,7 +12,7 @@ for load in 22 23; do
 		timeout "$1"s mpirun --allow-run-as-root --tag-output -bind-to none -map-by slot --mca osc ^ucx --mca btl_basverbose 50 --mca btl self,tcp --mca btl_tcp_if_include enp3s0f0 --mca pml ^ucx --mca mtl ^ofi --mca coll ^hcoll --np 3 -H liangyu:1,kasra:1,juno:1 -x NCCL_IB_DISABLE=1 -x NCCL_DEBUG=INFO -x NCCL_SOCKET_IFNAME=enp194s0 -x PATH -x LD_LIBRARY_PATH -x CONDA_DEFAULT_ENV -v python src/distributed_learning/pytorch_imagenet.py --synth -a resnet50 --epochs 1 -p 1 -b $batch_size > test_logs/"$load"G-3-"$batch_size" &
 		cd src/network/
 		sleep 19s
-		./client < configs/$dir/"$load"G_consist_to_"$2".config &> client_log_"$2" &
+		./client < configs/$dir/"$load"G_consist_to_"$2".config &> client_logs/"$load"G-3-"$batch_size" &
 		mkdir -p bws
 		cd ../..
 		sleep "$dir"s
--- traffic_logs/compute_packet_delay.py
+++ traffic_logs/compute_packet_delay.py
@@ -9,29 +9,66 @@ tcpdump_output_re = re.compile(r'(?P<ts>[0-9]+:[0-9]+:[0-9]+\.[0-9]+)\sIP\s(?P<s
 IP_TABLE = {"juno": "172.32.10.1",
             "kasra": "172.32.11.1",
             "liangyu": "172.32.12.1"}
+INVERTED_IP_TABLE = {v: k for k, v in IP_TABLE.items()}
 
 class Trace:
     def __init__(self, xp):
         self.flows = defaultdict(list)
         self.packet_delays = defaultdict(list)
         self.xp_identifier = xp
+        self.stats = {}
 
     def add_packet(self, packet):
         id_tuple = (packet.srcIP, packet.srcPort, packet.dstIP, packet.dstPort, packet.loggedAt)
-        if packet.loggedAt == packet.srcIP:
-            pair_tuple = (*id_tuple[:-1], packet.dstIP)
-        else:
-            pair_tuple = (*id_tuple[:-1], packet.srcIP)
         self.flows[id_tuple].append(packet)
-        if pair_tuple in self.flows:
-            try:
-                idx = self.flows[pair_tuple].index(packet)
-                self.packet_delays[packet.srcIP].append(abs((packet.time - self.flows[pair_tuple][idx].time).total_seconds()))
-                # if len(self.packet_delays[packet.srcIP]) % 1 == 0:
-                    # print("Found {} packet pairs from {}".format(len(self.packet_delays[packet.srcIP]), packet.srcIP))
-            except ValueError:
-                pass
 
+    def in_order_sanity_check(self):
+        for flow in self.flows:
+            for i in range(len(self.flows[flow]) - 1):
+                assert self.flows[flow][i + 1].time >= self.flows[flow][i].time, "{}".format((self.flows[flow][i+1], self.flows[flow][i]))
+        print("Flow was logged in order")
+
+    def match_packets(self):
+        print(set((k[:-1]) for k in self.flows.keys()))
+        for id_tuple in set((k[:-1]) for k in self.flows.keys()):
+            delays = []
+            retransmits = 0
+            total_packet_pairs = 0
+            unmatch_received_packets = 0
+            first_match = False
+            packets_before_first_match = 0
+            src_flow = self.flows[(*id_tuple, id_tuple[0])]
+            dst_flow = self.flows[(*id_tuple, id_tuple[2])]
+            print(id_tuple, len(src_flow), len(dst_flow))
+            i_s = 0
+            for i_d, p in enumerate(dst_flow):
+                # print(i_s, i_d)
+                for i_cur in range(i_s, len(src_flow)):
+                    if src_flow[i_cur].time > p.time + datetime.timedelta(milliseconds=10):
+                        if first_match:
+                            unmatch_received_packets += 1
+                        else:
+                            packets_before_first_match += 1
+                        break
+                    if src_flow[i_cur] == p:
+                        retransmits += i_cur - i_s
+                        i_s = i_cur + 1
+                        total_packet_pairs += 1
+                        delays.append((p.time - src_flow[i_cur].time).total_seconds())
+                        first_match = True
+                        break
+
+            self.packet_delays[id_tuple] = delays
+            self.stats[id_tuple] = {"retransmits": retransmits,
+                                    "total_packet_pairs": total_packet_pairs,
+                                    "unmatch_received_packets": unmatch_received_packets,
+                                    "before_first_match": packets_before_first_match}
+            if retransmits + total_packet_pairs == len(src_flow) and unmatch_received_packets + total_packet_pairs + packets_before_first_match == len(dst_flow):
+                print("Flow OK")
+            else:
+                print("Flow does not add up")
+                print(self.stats)
+            
     def show_stats(self):
         print("XP:{}, means: {}, global mean: {}, number of logged packets: {}\n".format(self.xp_identifier, list(map(np.mean, self.packet_delays.values())), np.mean([v for d in self.packet_delays for v in self.packet_delays[d]]), sum([len(v) for v in self.packet_delays.values()])))
 class Packet:
@@ -51,7 +88,11 @@ class Packet:
         return self.srcIP == other.srcIP and self.dstIP == other.dstIP and self.srcPort == other.srcPort\
                and self.dstPort == other.dstPort and self.seq_start == other.seq_start and self.seq_end == other.seq_end
 
+    def __str__(self):
+        return "{}: {}, {}.{} -> {}.{}, {}:{}".format(INVERTED_IP_TABLE[self.loggedAt], self.time, INVERTED_IP_TABLE[self.srcIP], self.srcPort, INVERTED_IP_TABLE[self.dstIP], self.dstPort, self.seq_start, self.seq_end)
 
+    def __repr__(self):
+        return self.__str__()
 def experiment(identifier, servers):
     trace = Trace(identifier)
     for server in servers:
@@ -84,7 +125,7 @@ def experiment(identifier, servers):
                 # if i % 1000 == 0:
                     # print("time for 1000 iteration: ", time() - start)
                     # start = time()
-                if i > 10000:
+                if i > 20000:
                     break
         finally:
             remote_file.close()
@@ -93,4 +134,6 @@ def experiment(identifier, servers):
     return trace
 
 trace = experiment(sys.argv[1], ["juno", "kasra", "liangyu"])
+# trace.in_order_sanity_check()
+trace.match_packets()
 trace.show_stats()
